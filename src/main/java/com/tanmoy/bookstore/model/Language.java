d94c92299896b38c98955b72e492adb13b6f2e16
unknown <tanmoy.official@gmail.com>
2018-06-08 14:32:07 +0530

    Module 4 - Entity created using javax.persistence.Entity

---
+++ src/main/java/com/tanmoy/bookstore/model/Language.java
@@ -0,0 +1,7 @@
+package com.tanmoy.bookstore.model;
+
+public enum Language {
+	
+	ENGLISH, FRENCH, SPANISH, ITALIAN, RUSSIAN;
+
+}
---
+++ src/main/java/com/tanmoy/bookstore/model/book.java
@@ -0,0 +1,95 @@
+package com.tanmoy.bookstore.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class book {
+	@Id @GeneratedValue
+	private Long id;
+	
+	@Column(length = 200)
+	private String title;
+	
+	@Column(length= 1000)
+	private String description;
+	
+	@Column(name = "unit_cost")
+	private Float unitCost;
+	
+	private String isbn;
+	
+	private Date publicationDate;
+	private Integer nbOfPages;
+	private String imageUrl;
+	private Language language;
+	
+	// list of getters and setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Float getUnitCost() {
+		return unitCost;
+	}
+	public void setUnitCost(Float unitCost) {
+		this.unitCost = unitCost;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public Date getPublicationDate() {
+		return publicationDate;
+	}
+	public void setPublicationDate(Date publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+	public Integer getNbOfPages() {
+		return nbOfPages;
+	}
+	public void setNbOfPages(Integer nbOfPages) {
+		this.nbOfPages = nbOfPages;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public Language getLanguage() {
+		return language;
+	}
+	public void setLanguage(Language language) {
+		this.language = language;
+	}
+	@Override
+	public String toString() {
+		return "book [getId()=" + getId() + ", getTitle()=" + getTitle() + ", getDescription()=" + getDescription()
+				+ ", getUnitCost()=" + getUnitCost() + ", getIsbn()=" + getIsbn() + ", getPublicationDate()="
+				+ getPublicationDate() + ", getNbOfPages()=" + getNbOfPages() + ", getImageUrl()=" + getImageUrl()
+				+ ", getLanguage()=" + getLanguage() + "]";
+	}
+
+
+}
